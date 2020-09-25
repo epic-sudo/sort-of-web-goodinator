ec751451b20e16822c988f459fa5e88f0f427230
tedscode <65257768+tedscode@users.noreply.github.com>
2020-09-24 20:42:41 -0500

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,4 @@
+styleElement = document.createElement('style');
+styleElement.setAttribute('type', 'text/css');
+styleElement.innerHTML = '@import url(\'https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap\'); *,body,h1,h2,h3,h4,h5,h6,p,a,div,#video-title.ytd-rich-grid-media,.docs-menubar,#video-title.ytd-rich-grid-media,.goog-toolbar-menu-button-caption,.goog-inline-block:not(.material-icons) { font-family: \'Comic Neue\', cursive !important; font-weight: bold !important; }';
+document.head.appendChild(styleElement);
