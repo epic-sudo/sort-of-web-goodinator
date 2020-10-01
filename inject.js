a49ab4c2f0c8bd1d8d434ce634d3bfae4d2c0ba0
epic-sudo <65257768+epic-sudo@users.noreply.github.com>
2020-10-01 18:50:57 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,4 +1,4 @@
 styleElement = document.createElement('style');
 styleElement.setAttribute('type', 'text/css');
-styleElement.innerHTML = '@import url(\'https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap\'); *:not(.material-icons) { font-family: \'Comic Neue\', cursive !important; font-weight: bold !important; }';
+styleElement.innerHTML = '@import url(\'https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap\'); *:not(i):not(.material-icons):not(svg):not(path):not(span) { font-family: \'Comic Neue\', cursive !important; font-weight: bold !important; }';
 document.head.appendChild(styleElement);
